93d9925535466e8d2bb8433b78bcfbfa276246d1
Idlehumor <craig_saunders@hotmail.com>
2021-02-24 01:45:08 -0500

    Committing week one tuesday Shapes assignment

---
+++ com.ss.craig/src/com/ss/craig/week/one/tuesday/shapes/ShapesMain.java
@@ -0,0 +1,21 @@
+/**
+ * Tuesday Assignment: Create shape hierarchy
+ */
+package com.ss.craig.week.one.tuesday.shapes;
+
+/**
+ * @author Craig Saunders
+ *
+ */
+public class ShapesMain {
+
+    /**
+     * @param args
+     */
+    public static void main(String[] args) {
+        new Rectangle(2.5d, 5d);
+        new Circle(15.124d);
+        new Triangle(540d,1200d);
+    }
+
+}
