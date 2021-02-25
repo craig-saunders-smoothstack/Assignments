82bca688d718510c7c94a1f2bf2c41d40719d278
Craig Saunders <craig.saunders@smoothstack.com>
2021-02-25 08:40:07 -0500

    AppendTextFile assignment week one commit

---
+++ com.ss.craig/src/com/ss/craig/week/one/wednesday/appendtextfile/Program.java
@@ -0,0 +1,22 @@
+/**
+ * Wednesday Assignment: Program to append text to the end of an existing file
+ */
+package com.ss.craig.week.one.wednesday.appendtextfile;
+
+/**
+ * @author Craig Saunders
+ *
+ */
+public class Program {
+
+    /**
+     * @param args 
+     * @args[0] : file path
+     * @args[1] : text to append
+     */
+    public static void main(String[] args)
+    {
+        AppendTextFile.appendText(args);
+    }
+
+}
