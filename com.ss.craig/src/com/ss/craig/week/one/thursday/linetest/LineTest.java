2d9ab1de5a7ad003797b8abc8b7c614bd8665171
Craig Saunders <craig.saunders@smoothstack.com>
2021-02-26 12:11:30 -0500

    Finished Linetest assginment

--- com.ss.craig/src/com/ss/craig/week/one/thursday/linetest/LineTest.java
+++ com.ss.craig/src/com/ss/craig/week/one/thursday/linetest/LineTest.java
@@ -16,18 +16,23 @@ import org.junit.Test;
 public class LineTest {
     Line line_one;
     Line line_two;
+    Line line_three;
+    Line line_four;
     
     @Before
     public void setup()
     {
         line_one = new Line(2d,2d,1d,1d);
         line_two = new Line(3d,3d,202.01d,202d);
+        line_three = new Line(3d,3d,3d,3d);
+        line_four = new Line(3d,202d,202d,3d);
     }
     
     @Test
     public void testGetSlope()
     {
         assertEquals(line_one.getSlope(), 1d, 0.0001d);
+        assertEquals(line_three.getSlope(), 0, 0.0001d);
     }
     
     @Test
@@ -40,5 +45,6 @@ public class LineTest {
     public void testParallelTo()
     {
         assertTrue(line_one.parallelTo(line_two));
+        assertTrue(line_one.parallelTo(line_four));
     }
 }
