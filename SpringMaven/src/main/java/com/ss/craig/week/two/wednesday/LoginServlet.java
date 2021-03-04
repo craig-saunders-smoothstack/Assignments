d5e32ab18f37e73efc9fe52fa0706534d31690b0
Craig Saunders <craig.saunders@smoothstack.com>
2021-03-04 16:03:33 -0500

    Redirected to a bad login html for bad logins
    Added status codes and contenttype

--- SpringMaven/src/main/java/com/ss/craig/week/two/wednesday/LoginServlet.java
+++ SpringMaven/src/main/java/com/ss/craig/week/two/wednesday/LoginServlet.java
@@ -4,7 +4,6 @@ import java.io.IOException;
 import java.io.PrintWriter;
 
 import javax.servlet.ServletException;
-import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
@@ -12,9 +11,10 @@ import javax.servlet.http.HttpServletResponse;
 /**
  * Servlet implementation class LoginServlet
  */
-@WebServlet("/LoginServlet")
+//@WebServlet("/LoginServlet") -- added to web.xml so this is not needed anymore
 public class LoginServlet extends HttpServlet {
     private static final long serialVersionUID = 1L;
+    // Hardcoded password and username to test the servlet functionality
     private static final String hardcoded_username = "craig.saunders@smoothstack.com";
     private static final String hardcoded_password = "java-feb-2021";
 
@@ -32,6 +32,8 @@ public class LoginServlet extends HttpServlet {
      */
     protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException
     {
+        response.setStatus(200);
+        response.setContentType("text/html");
         response.getWriter().append("This Servlet does not take get requests.");
     }
 
@@ -39,8 +41,10 @@ public class LoginServlet extends HttpServlet {
      * @see HttpServlet#doPost(HttpServletRequest request, HttpServletResponse
      *      response)
      */
-    protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException
+    protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException, IllegalStateException
     {
+        response.setStatus(200);
+        response.setContentType("text/html");
         String username = request.getParameter("SpringMaven-Username");
         String password = request.getParameter("SpringMaven-Password");
         PrintWriter writer = response.getWriter();
@@ -51,8 +55,7 @@ public class LoginServlet extends HttpServlet {
         }
         else
         {
-            writer.println("<h2>Wrong username or password!</h2>");
-            writer.println("<h3>Please go back and try again.</h3>");
+            response.sendRedirect("./bad_login.html");
         }
         writer.close();
     }
