f9ab95734d0d4b1f968d9a82266f8635835defd2
Idlehumor <craig_saunders@hotmail.com>
2021-03-04 11:43:25 -0500

    Week two Wednesday assignment

---
+++ SpringMaven/src/main/java/com/ss/craig/week/two/wednesday/LoginServlet.java
@@ -0,0 +1,54 @@
+package com.ss.craig.week.two.wednesday;
+
+import java.io.IOException;
+import java.io.PrintWriter;
+
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+/**
+ * Servlet implementation class LoginServlet
+ */
+@WebServlet("/LoginServlet")
+public class LoginServlet extends HttpServlet {
+	private static final long serialVersionUID = 1L;
+	private static final String hardcoded_username = "craig.saunders@smoothstack.com";
+    private static final String hardcoded_password = "java-feb-2021";   
+    /**
+     * @see HttpServlet#HttpServlet()
+     */
+    public LoginServlet() {
+        super();
+    }
+
+	/**
+	 * @see HttpServlet#doGet(HttpServletRequest request, HttpServletResponse response)
+	 */
+	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		String username = request.getParameter("SpringMaven-Username");
+		String password = request.getParameter("SpringMaven-Password");
+		PrintWriter writer = response.getWriter();
+		if (hardcoded_username.equals(username) &&  hardcoded_password.equals(password))
+		{
+    		writer.println("<h2>Thank you "+username+"</h2>");
+            writer.println("<h3>You are now logged in!</h3>");
+		}
+		else
+		{
+            writer.println("<h2>Wrong username or password!</h2>");
+            writer.println("<h3>Please go back and try again.</h3>");		    
+		}
+        writer.close();
+	}
+
+	/**
+	 * @see HttpServlet#doPost(HttpServletRequest request, HttpServletResponse response)
+	 */
+	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		doGet(request, response);
+	}
+
+}
