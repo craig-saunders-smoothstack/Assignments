1b0a95ea374ae8e3e391038377a84b611b0efe1c
Craig Saunders <craig.saunders@smoothstack.com>
2021-03-08 18:48:13 -0500

    Removed default security and implemented database backed security

--- UtopiaAirlines/src/main/java/com/ss/craig/week/two/weekend/assignment/config/UtopiaWebSecurityConfig.java
+++ UtopiaAirlines/src/main/java/com/ss/craig/week/two/weekend/assignment/config/UtopiaWebSecurityConfig.java
@@ -3,10 +3,10 @@
  */
 package com.ss.craig.week.two.weekend.assignment.config;
 
-import org.springframework.beans.factory.annotation.Autowired;
+//import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
+//import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
@@ -20,7 +20,7 @@ import org.springframework.security.crypto.password.PasswordEncoder;
 @Configuration
 @EnableWebSecurity
 public class UtopiaWebSecurityConfig extends WebSecurityConfigurerAdapter {
-    
+/*    
     @Autowired
     public void configure(AuthenticationManagerBuilder auth) throws Exception {
         // Hardcoded logins for default login testing purposes
@@ -31,7 +31,7 @@ public class UtopiaWebSecurityConfig extends WebSecurityConfigurerAdapter {
           .and()
           .withUser("Akbar").password(passwordEncoder().encode("Is#1!ssTrainer2021")).roles("USER");
     }
-    
+*/    
     @Override
     protected void configure(HttpSecurity http) throws Exception {
         http
---
+++ UtopiaAirlines/src/main/java/com/ss/craig/week/two/weekend/assignment/controllers/DatabaseUserDetailsService.java
@@ -0,0 +1,49 @@
+/**
+ * 
+ */
+package com.ss.craig.week.two.weekend.assignment.controllers;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+
+import org.springframework.stereotype.Service;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.core.GrantedAuthority;
+import org.springframework.security.core.authority.SimpleGrantedAuthority;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
+
+import com.ss.craig.week.two.weekend.assignment.jpaentities.User;
+import com.ss.craig.week.two.weekend.assignment.jpaentities.UserRole;
+import com.ss.craig.week.two.weekend.assignment.repositories.UserRepository;
+import com.ss.craig.week.two.weekend.assignment.repositories.UserRoleRepository;
+
+/**
+ * @author Craig Saunders
+ *
+ */
+@Service
+public class DatabaseUserDetailsService implements UserDetailsService {
+
+    @Autowired
+    private UserRepository user_repo;
+
+    @Autowired
+    private UserRoleRepository user_role_repo;
+    
+    @Override
+    public UserDetails loadUserByUsername(String username) throws UsernameNotFoundException
+    {
+        User user = user_repo.findByUsername(username);
+        if (user == null) {
+            throw new UsernameNotFoundException(username);
+        }
+        UserRole user_role = user_role_repo.findById(user.getUserRole().getId());
+        List<GrantedAuthority> grantedAuthorities = new ArrayList<>();
+        Arrays.asList(user_role.getName().split(" ")).stream().forEach(r -> grantedAuthorities.add(new SimpleGrantedAuthority(r)));
+        return new org.springframework.security.core.userdetails.User(user.getUsername(), user.getPassword(), grantedAuthorities);
+    }
+
+}
